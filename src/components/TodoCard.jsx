1531e95274c65a40ce28691b2b8fa3ce6e726b37
ksljivo1 <ksljivo1@etf.unsa.ba>
2024-10-17 11:23:27 +0200

    Initial commit

---
+++ src/components/TodoCard.jsx
@@ -0,0 +1,25 @@
+export function TodoCard(props) {
+    const {todo, handleDeleteTodo, todoIndex, handleCompleteTodo} = props
+
+    return (
+        <div className="card todo-item">
+            <p>{todo.input}</p>
+            <div className="todo-buttons">
+                <button onClick={
+                    () => {
+                        handleCompleteTodo(todoIndex)
+                    }
+                } disabled={todo.complete}>
+                    <h6>Done</h6>
+                </button>
+                <button>
+                    <h6 onClick={
+                        () => {
+                            handleDeleteTodo(todoIndex)
+                        }
+                    }>Delete</h6>
+                </button>
+            </div>
+        </div>
+    )
+}
